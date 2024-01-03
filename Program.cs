ef44f3576523c0e5084f2fdc0b457104a40a8976
Dakota Johnson <47164434+D7K7@users.noreply.github.com>
2024-01-02 21:58:46 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+class Permutations
+{
+   public static List<string> SinglePermutations(string s)
+   {
+      if( s.Length == 1 ) return new List<string>() {s};
+   }
+}
